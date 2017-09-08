4f14136d2242f20511ea303ea24f20c095dfad16
zhaochuanzhi <1570540016@qq.com>
2017-09-08 13:59:56 +0800

    Initial commit

---
+++ src/main/java/com/example/springBootDemo/SpringBootDemo01Application.java
@@ -0,0 +1,12 @@
+package com.example.springBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo01Application.class, args);
+	}
+}
